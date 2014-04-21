1bfea2e0c6fd1893e7227bd52db72259063e7a9c
Brian Murphy <bmurphy1@mac.com>
2014-04-21 13:12:26 -0400

    Jobs edit route/view templates complete

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,4 +6,14 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(name: params[:job][:name], command: params[:job][:command])
+    redirect_to job_path(@job)
+  end
 end
