4b69dc4086c5b057402360afe2f79d99d15e9614
Brian Murphy <bmurphy1@mac.com>
2014-04-23 14:45:02 -0400

    Refactor scheduling functionality to Job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,7 +5,21 @@ class Job < ActiveRecord::Base
   validates :name, uniqueness: true
   validates :name, :command, presence: true
 
+  after_save do
+    if self.schedule
+      execute
+    end
+  end
+
   def execute
-    system(self.command)
+    Resque.remove_schedule(self.name)
+    JobLog.create(job_id: self.id)
+    config = {}
+    config[:class] = 'ResqueWorker'
+    config[:args] = self.command
+    config[:cron] = self.schedule.cron_string
+    config[:queue] = 'high'
+    config[:persist] = true
+    Resque.set_schedule(self.name, config)
   end
 end
