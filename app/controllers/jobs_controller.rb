b315405d1d3188ff97e3618e3fefae7e6bdd064b
Brian Murphy <bmurphy1@mac.com>
2014-04-26 15:40:03 -0400

    Add New and Create routes for Job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,6 +12,15 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.create(name: params[:job][:name], command: params[:job][:command])
+    redirect_to job_path(@job)
+  end
+
   def update
     @job = Job.find(params[:id])
     @schedule = Schedule.find(params[:job][:schedule])
