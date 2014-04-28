baaebcf323f6e751f1ff04cb0200854f3ef2a28a
Brian Murphy <bmurphy1@mac.com>
2014-04-28 09:53:49 -0400

    Refactor JobLog model to denormalize attributes

--- db/migrate/20140421011857_create_job_logs.rb
+++ db/migrate/20140421011857_create_job_logs.rb
@@ -1,9 +1,9 @@
 class CreateJobLogs < ActiveRecord::Migration
   def change
     create_table :job_logs do |t|
-      t.belongs_to :job
-      t.belongs_to :user
-      t.belongs_to :schedule
+      t.string :name
+      t.string :command
+      t.string :schedule
       t.boolean :success
       t.timestamps
     end
