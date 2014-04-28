c38f3fbea2c298f6a98ec89e4ed51407465b6775
Brian Murphy <bmurphy1@mac.com>
2014-04-28 11:30:19 -0400

    Code cleanup

--- app/models/resque_worker.rb
+++ app/models/resque_worker.rb
@@ -3,10 +3,7 @@ class ResqueWorker < ActiveRecord::Base
   @queue = :high
 
   def self.perform(job_options)
-    p job_options
-    p job_options.class
-    # run command, saving success state
-    # create JobLog entry from job and success state
+    # job_options comes in as JSON
     success_result = system(job_options["command"])
     JobLog.create(name: job_options["name"], command: job_options["command"], schedule: job_options["schedule"], success: success_result)
   end
