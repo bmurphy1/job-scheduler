e9f9bebecafd5564f27747b4801adbbf61c9cc02
Brian Murphy <bmurphy1@mac.com>
2014-04-29 11:28:06 -0400

    Clean up views, add css

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @job = Job.create(name: "Test job", command: "touch test.txt")
+    @schedule = Schedule.create(name: "Every day at noon", cron_string: "0 12 * * *")
+  end
+
+  after :each do
+    Resque.remove_schedule(@job.name)
+    @job.destroy
+    @schedule.destroy
+  end
+
+  it "Gets scheduled when associated with a schedule" do
+    @job.update(schedule: @schedule)
+    expect(Resque.schedule[@job.name]).to be_true
+  end
+
+  xit "Gets unscheduled when unassociated with a schedule" do
+    @job.update(schedule: @schedule)
+    @job.update(schedule: nil)
+    expect(Resque.schedule[@job.name]).to be_nil
+  end
 end
